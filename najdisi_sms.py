a5334680e8b8c797f60c7b2e41836c475113be6b
Andraz Brodnik <brodul@brodul.org>
2014-01-13 00:11:46 +0100

    Add logging and refactor.

--- najdisi_sms.py
+++ najdisi_sms.py
@@ -7,8 +7,13 @@ import urllib
 import urllib2
 import cookielib
 from optparse import OptionParser
+import logging
 
 import mechanize
+
+logging.basicConfig()
+log = logging.getLogger("najdisi_sms")
+log.setLevel(logging.INFO)
 
 
 def main():
@@ -30,90 +35,139 @@ def main():
         "--useragent",
         dest="useragent",
         help=u"HTTP User Agent",
-        default="Mozilla/5.0 (Windows; U; Windows NT 6.1; es-ES; rv:1.9.2.3)" +
-        "Gecko/20100401 Firefox/3.6.3"
+        default="Mozilla/5.0 (Windows; U; Windows NT 6.1; es-ES; rv:1.9.2.3)"
+        + "Gecko/20100401 Firefox/3.6.3"
     )
     (options, args) = parser.parse_args()
     who = args.pop(0)
     msg = ' '.join(args)
 
-    # don't change
-    temp_recipent = who.strip().replace(' ', '')[3:]
-    recipent = temp_recipent[:3] + ' ' + temp_recipent[3:]
-    base_code = int(who[1:3])
+    sender = SMSSender(options.username, options.password, options.useragent)
+    sender.sent(who, msg)
 
-    if len(msg) > 160:
-        raise Exception('predolgo sporocilo')
 
-    print 'Omrezna koda: %s' % base_code
-    print 'Prejemnik: %s' % recipent
-    print 'Sporocilo: %s' % msg
-    print '\nPosiljam sms ...'
+class SMSSender(object):
+    """Docstring for SMSSender. """
 
-    # handlers and headers
-    jar = cookielib.CookieJar()
-    handler = urllib2.HTTPCookieProcessor(jar)
-    handler2 = urllib2.HTTPHandler()
-    handler3 = urllib2.HTTPSHandler()
-    handler4 = mechanize.HTTPRefererProcessor()
-    opener = urllib2.build_opener(handler3, handler2, handler, handler4)
-    opener.addheaders = [
-        ("User-agent", options.useragent),
-    ]
-    urllib2.install_opener(opener)
+    def __init__(self, username, password, useragent=""):
+        """@todo: to be defined1. """
+        self.username = username
+        self.password = password
+        da = "Mozilla/5.0 (Windows; U; Windows NT 6.1; es-ES; rv:1.9.2.3)" \
+            + "Gecko/20100401 Firefox/3.6.3"
+        self.useragent = useragent or da
 
-    # authentication
-    data = urllib.urlencode({
-        'j_username': options.username,
-        'j_password': options.password,
-        '_spring_security_remember_me': 'on',
-    })
-    urllib2.urlopen("https://id.najdi.si/j_spring_security_check", data)
+    def normalize_receiver(self, receiver_num):
+        """@todo: Docstring for normalize_receiver.
 
-    # timestamp cookie
-    chkcookie = str(time.time()).replace('.', '')
-    while len(chkcookie) < 13:
-        chkcookie += '0'
+        :receiver_num: @todo
+        :returns: @todo
 
-    data = urllib.urlencode({
-        'sms_action': '4',
-        'sms_so_ac_%s' % chkcookie: base_code,
-        'sms_so_l_%s' % chkcookie: recipent,
-        'myContacts': '',
-        'sms_message_%s' % chkcookie: msg.strip(),
-    })
+        """
+        who = receiver_num
 
-    req = urllib2.Request("http://www.najdi.si/sms/smsController.jsp?" + data)
-    cookie_tuple = jar._normalized_cookie_tuples([
-        [('chkcookie', chkcookie),
-        ('domain', 'www.najdi.si')],
-    ])
-    cookie = jar._cookie_from_cookie_tuple(cookie_tuple[0], req)
-    jar.set_cookie(cookie)
-    cookie_tuple = jar._normalized_cookie_tuples([
-        [('lganchor', 'freesms'),
-        ('domain', 'www.najdi.si')],
-    ])
-    cookie = jar._cookie_from_cookie_tuple(cookie_tuple[0], req)
-    jar.set_cookie(cookie)
+        # don't change
+        temp_recipent = who.strip().replace(' ', '')[3:]
+        recipent = temp_recipent[:3] + ' ' + temp_recipent[3:]
+        base_code = int(who[1:3])
 
-    # second step authentication
-    urllib2.urlopen(
-        "http://www.najdi.si/auth/login.jsp" +
-        "?sms=1&target_url=http%3A%2F%2Fwww.najdi.si%2Findex.jsp"
-    )
+        return base_code, recipent
 
-    # validate authentication
-    req.add_header('X-Requested-With', 'XMLHttpRequest')
-    urllib2.urlopen("http://www.najdi.si/auth/isPrincipalAlive.jsp")
+    def check_msg_leng(self, msg):
+        """@todo: Docstring for check_msg_leng.
 
-    # send sms
-    r = urllib2.urlopen(req)
-    d = json.loads(r.read())
-    if d['dialog'] == 3:
-        print 'Uspelo. Preostalih smsov danes: %d' % d['msg_left']
-    else:
-        print 'Napaka: %r' % d
+        :msg: @todo
+        :returns: @todo
+
+        """
+        if len(msg) > 160:
+            raise Exception('predolgo sporocilo')
+
+        return msg
+
+    def sent(self, receiver, msg):
+        """@todo: Docstring for sent.
+
+        :receiver: @todo
+        :msg: @todo
+        :returns: @todo
+
+        """
+
+        msg = self.check_msg_leng(msg)
+
+        base_code, recipent = self.normalize_receiver(receiver)
+
+        log.info('Omrezna koda: %s', base_code)
+        log.info('Prejemnik: %s', recipent)
+        log.info('Sporocilo: %s',  msg)
+        log.info('Posiljam sms ...')
+
+        # handlers and headers
+        jar = cookielib.CookieJar()
+        handler = urllib2.HTTPCookieProcessor(jar)
+        handler2 = urllib2.HTTPHandler()
+        handler3 = urllib2.HTTPSHandler()
+        handler4 = mechanize.HTTPRefererProcessor()
+        opener = urllib2.build_opener(handler3, handler2, handler, handler4)
+        opener.addheaders = [
+            ("User-agent", self.useragent),
+        ]
+        urllib2.install_opener(opener)
+
+        # authentication
+        data = urllib.urlencode({
+            'j_username': self.username,
+            'j_password': self.password,
+            '_spring_security_remember_me': 'on',
+        })
+        urllib2.urlopen("https://id.najdi.si/j_spring_security_check", data)
+
+        # timestamp cookie
+        chkcookie = str(time.time()).replace('.', '')
+        while len(chkcookie) < 13:
+            chkcookie += '0'
+
+        data = urllib.urlencode({
+            'sms_action': '4',
+            'sms_so_ac_%s' % chkcookie: base_code,
+            'sms_so_l_%s' % chkcookie: recipent,
+            'myContacts': '',
+            'sms_message_%s' % chkcookie: msg.strip(),
+        })
+
+        req = urllib2.Request("http://www.najdi.si/sms/smsController.jsp?" + data)
+        cookie_tuple = jar._normalized_cookie_tuples([
+            [('chkcookie', chkcookie),
+            ('domain', 'www.najdi.si')],
+        ])
+        cookie = jar._cookie_from_cookie_tuple(cookie_tuple[0], req)
+        jar.set_cookie(cookie)
+        cookie_tuple = jar._normalized_cookie_tuples([
+            [('lganchor', 'freesms'),
+            ('domain', 'www.najdi.si')],
+        ])
+        cookie = jar._cookie_from_cookie_tuple(cookie_tuple[0], req)
+        jar.set_cookie(cookie)
+
+        # second step authentication
+        urllib2.urlopen(
+            "http://www.najdi.si/auth/login.jsp" +
+            "?sms=1&target_url=http%3A%2F%2Fwww.najdi.si%2Findex.jsp"
+        )
+
+        # validate authentication
+        req.add_header('X-Requested-With', 'XMLHttpRequest')
+        urllib2.urlopen("http://www.najdi.si/auth/isPrincipalAlive.jsp")
+
+        # send sms
+        r = urllib2.urlopen(req)
+        d = json.loads(r.read())
+        if d['dialog'] == 3:
+            log.info('Uspelo. Preostalih smsov danes: %d',  d['msg_left'])
+        else:
+            log.info('Napaka: %r', d)
+
 
 if __name__ == '__main__':
     main()
